e22227b49e34d994b4b001d48e088872f20d598c
Sahana Balram <sahana2008balram@gmail.com>
2017-09-26 23:16:29 -0700

    Fix styling

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,12 +6,12 @@ DROP TABLE burgers;
 INSERT INTO burgers(burger_name,devoured,date)
 VALUES("Cheese Burger",true,'2017-09-25'),("Cheese and Bacon Burger",true,'2017-09-25'),("Fillet Burger",true,'2017-09-25');
 
-SELECT * FROM burgers;
+
 
 INSERT INTO burgers(burger_name,devoured,date) VALUES("Cheese Burger1",true,'2017-09-25');
 
 INSERT INTO burgers (burger_name,devoured,date) VALUES("beet",false,"2017-09-25");
 
-DELETE FROM  burgers WHERE id= 8;
+DELETE FROM  burgers WHERE id= 9;
 
-UPDATE burgers SET devoured = false WHERE id=9;
+UPDATE burgers SET devoured = true WHERE id=9;
