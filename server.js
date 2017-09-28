940946984af1aec3f42d1df55203f361e56d154e
Sahana Balram <sahana2008balram@gmail.com>
2017-09-27 21:57:26 -0700

    Add code for import route and use body-parser in server.js file

--- server.js
+++ server.js
@@ -5,67 +5,25 @@ var connection = require("./config/connection.js");
 var burger = require("./models/burger.js")
 var orm = require("./config/orm.js");
 var app = express();
+var ROUTERAPI = require("./controllers/burgers_controller.js");
 app.use(express.static("public"));
-var port = 3001;
+
+var PORT = 3001;
 
 // Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use('/', ROUTERAPI);
 
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({
     defaultLayout: "main"
 }));
-var notEaten = [];
-var eaten = [];
+
 app.set("view engine", "handlebars");
-app.get("/", function (request, response) {
-    connection.query("SELECT * FROM burgers", function (error, data) {
-        if (error) {
-            console.log(error);
-            throw error;
-        }
-        for (var i = 0; i < data.length; i++) {
-            if (data[i].devoured === true) {
-                eaten.push(data[i]);
-            } else {
-                notEaten.push(data[i]);
-            }
-        }
-        response.render("index", {
-            burgers: data
-        });
-    });
+app.listen(PORT, function () {
+	console.log("App listening on PORT " + PORT);
 });
-
-app.post("/", function (req, res) {
-    // Test it
-    console.log('You sent, ' + req.body.burger);
-
-    // Test it
-    // res.send('You sent, ' + req.body.task);
-    var query = "INSERT INTO burgers (burger_name) VALUES(?)";
-    connection.query(query, [req.body.burger], function (err, result) {
-        if (err) throw err;
-        res.redirect("/");
-    });
-});
-
-app.put("/", function (request, response) {
-    console.log("receive put request");
-    var query = 'UPDATE burgers SET devoured = ' 
-    query += request.body.devoured;
-    query += ' WHERE id=';
-    query += request.body.id;
-    console.log(query);
-    connection.query(query, function (error, result) {
-        if (error) {
-            response.json({"error": erro});
-        }
-        response.json({"error": "none"});
-    });
-});
-
-app.listen(port);
