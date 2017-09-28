bfc7db64391bcbe77b0625e904a974738b220e96
Sahana Balram <sahana2008balram@gmail.com>
2017-09-28 16:40:08 -0700

    Add deployment code for database

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,21 @@
 var mysql = require("mysql");
 var connection;
-// create database connection
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWDB_URL) {
+    connection = mysql.createConnection(process.env.JAWDB_URL);
 } else {
-    connection = mysql.createConnection({
+    mysql.createConnection({
         host: "localhost",
         port: 3306,
-       // username
+        // username
         user: "root",
         // password
         password: process.env.MYSQL_PASSWORD,
         // database name
         database: "burgers_db"
     });
-}
+};
+// create database connection
 
 connection.connect();
+
 module.exports = connection;
