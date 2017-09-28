912cc7fa98520b8bdc2de3f0dfc0f876268368fa
Sahana Balram <sahana2008balram@gmail.com>
2017-09-28 10:02:38 -0700

    Add environment for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 var mysql = require("mysql");
-
+var connection;
 // create database connection
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-   // username
-    user: "root",
-    // password
-    password: process.env.MYSQL_PASSWORD,
-    // database name
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+       // username
+        user: "root",
+        // password
+        password: process.env.MYSQL_PASSWORD,
+        // database name
+        database: "burgers_db"
+    });
+}
 
+connection.connect();
 module.exports = connection;
