efd7541fe2c91f8784a6cc9ef13c9d5832135310
Sahana Balram <sahana2008balram@gmail.com>
2017-09-27 21:53:20 -0700

    Fix the router for get, push and put

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,22 +5,37 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required
 
-router.get("/",function(request,response){
-    burger.all(function(data){
-        var hbsObject = {
-            burger:data
-        };
-        console.log(hbsObject);
-        res.render("index",hbsObject);
+router.get("/", function (request, response) {
+  burger.selectAll(function (data) {
+    var notEaten = [];
+    var eaten = [];
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].devoured === 1) {
+        eaten.push(data[i]);
+      } else {
+        notEaten.push(data[i]);
+      }
+    }
+    response.render("index", {
+      eaten: eaten,
+      noteaten: notEaten
     });
+
+  });
+});
+router.post("/", function (req, res) {
+  burger.insertOne([req.body.burger],function(result){
+    res.redirect("/");
+  });
 });
 
-router.post("/", function(req, res) {
-    cat.create([
-      "burger_name", "veggieBurger"
-    ], [
-      req.body.name, req.body.veggieBurger
-    ], function() {
-      res.redirect("/");
-    });
+router.put("/", function (request, response) {
+  var condition = "id = " + request.body.id;
+  burger.updateOne({
+    devoured: request.body.devoured
+  }, condition, function() {
+    response.redirect("/");
   });
+});
+
+module.exports = router;
