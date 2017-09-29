613caf898e143f900ac29f3f61f015ea5ada3d36
Sahana Balram <sahana2008balram@gmail.com>
2017-09-28 18:02:32 -0700

    Fix the connection.connect issue

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 var mysql = require("mysql");
 var connection;
+
 if (process.env.JAWDB_URL) {
     connection = mysql.createConnection(process.env.JAWDB_URL);
 } else {
-    mysql.createConnection({
+    connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         // username
@@ -13,9 +14,7 @@ if (process.env.JAWDB_URL) {
         // database name
         database: "burgers_db"
     });
-};
-// create database connection
-
+    
+}
 connection.connect();
-
 module.exports = connection;
